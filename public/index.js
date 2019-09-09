76ba11740c075ea833835d3631857327c492bf4e
unknown <398017990@qq.com>
2019-09-09 14:48:33 +0800

    添加本地测试服务器

--- public/index.js
+++ public/index.js
@@ -1,9 +1,15 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs-dist/build/pdf.worker.js';
 
 // let DEFAULT_URL = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
-let DEFAULT_URL = 'https://www.finra.org/sites/default/files/web-crd-view-individual.pdf';
+
+let DEFAULT_URL = './test2.pdf';
 
 let container = document.getElementById('pageContainer');
+
+let CSS_UNITS = 96 / 72;
+
+let width = document.body.clientWidth * 0.8;
+width > 1000 && (width = 1000);
 
 let loadingTask = pdfjsLib.getDocument({
     url: DEFAULT_URL,
@@ -11,13 +17,7 @@ let loadingTask = pdfjsLib.getDocument({
     cMapPacked: true,
 });
 
-let CSS_UNITS = 96 / 72;
-
-let width = document.body.clientWidth * 0.8;
-width > 1000 && (width = 1000);
-
 loadingTask.promise.then(function(pdfDoc) {
-
     for(let i=0; i<pdfDoc.numPages; i++){
         pdfDoc.getPage(i+1).then(function(pdfPage) {
 
