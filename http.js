46343786ed775584cba12e1d56c9146673d65b82
unknown <398017990@qq.com>
2019-11-06 09:43:30 +0800

    修改

--- http.js
+++ http.js
@@ -1,4 +1,16 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("public")).listen(8000);
+//设置允许跨域访问该服务.
+
+// app.all('*', function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+   
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Methods', '*');
+//     res.header('Content-Type', 'application/json;charset=utf-8');
+
+//     next();
+// });
+
+app.use(express.static("public")).listen(7000);
--- public/dist/index.js
+++ public/dist/index.js
@@ -9,60 +9,29 @@ let token = location.parse()["token"];
 
 // renderPDF("./2.pdf");
 
-function download_file(fileURL, fileName) {
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
-        save.download = fileName || filename;
-        if (navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
-            document.location = save.href; 
-        }else{
-            var evt = new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': false
-            });
-            save.dispatchEvent(evt);
-            (window.URL || window.webkitURL).revokeObjectURL(save.href);
-        }	
-    }else if ( !! window.ActiveXObject && document.execCommand)     {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL)
-        _window.close();
-    }
-}
-
-// download_file("./2.pdf");
 
 axios.get(`/edispatch/invoice/url/byToken?token=${token}`).then(function (response) {
-    let res = response.data;
-    if(!res || res.code != 100200){
-        alert(res.message);
-        return;
-    }
-    let {invoiceUrl, proxyUrl} = res.data || {};
+    let {invoiceUrl, proxyUrl} = response.data || {};
 
     let downloadBtn = document.querySelector(".downloadBtn");
-    downloadBtn.onclick = download_file(proxyUrl);
+    downloadBtn.href = "invoiceUrl";
 
     renderPDF(proxyUrl);
 
 }).catch(function (error) {
     console.log(error);
+    alert(error);
 });
 
 function renderPDF(url) {
-    pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs-dist/build/pdf.worker.min.js';
+    pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs-dist/build/pdf.worker.js';
 
     let container = document.getElementById('pageContainer');
     
     let CSS_UNITS = 96 / 72;
     
-    let width = document.body.clientWidth - 40;
-    width > 1192 && (width = 1192);
+    // let width = document.body.clientWidth * 0.8;
+    let width = 400;
     
     let loadingTask = pdfjsLib.getDocument({
         url: url,
